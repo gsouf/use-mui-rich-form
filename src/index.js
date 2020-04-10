e78e130adf18d4bbedc72b72ac4cad96dcb1057d
sghzal <sghzal@gmail.com>
2020-04-10 22:54:06 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export { default as useMuiRichForm } from "./useMuiRichForm";
 export { default as CircularButton } from "./CircularButton";
-export { default as Field } from "./Field";
+export { default as Field } from "./TextField";
 export { default as Form } from "./Form";
 export { default as Button } from './Button';
