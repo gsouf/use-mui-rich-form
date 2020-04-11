e098b64ed1205d04f6171fe6bdc1a6258689d068
sghzal <sghzal@gmail.com>
2020-04-11 20:04:01 +0200

    form load mask

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { CircularProgress } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
+    root: {
+      position: 'relative'
+    },
     fieldset: {
       margin: 0,
       padding: 0,
       border: 0,
     },
+    loading: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    spinner: {
+      background: `rgba(0, 0, 0, 0.5)`,
+      borderRadius: theme.borderRadius,
+      padding: theme.spacing(3)
+    }
   })
 );
 
@@ -17,10 +36,17 @@ export default function Form(props) {
   const { disabled, children, richForm, ...rest } = props;
 
   return (
-    <form {...rest} onSubmit={props.richForm.handleSubmit(props.onSubmit)}>
+    <form {...rest} className={classes.root} onSubmit={props.richForm.handleSubmit(props.onSubmit)}>
       <fieldset className={classes.fieldset} disabled={richForm.readOnly}>
         {children}
       </fieldset>
+      {richForm.loading && (
+        <div className={classes.loading}>
+          <div className={classes.spinner}>
+            <CircularProgress/>
+          </div>
+        </div>
+      )}
     </form>
   );
 }
