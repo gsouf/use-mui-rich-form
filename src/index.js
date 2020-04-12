e797d5e50d426bffae493386796ed00477c7c700
sghzal <sghzal@gmail.com>
2020-04-12 15:47:10 +0200

    bump

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { default as CircularButton } from "./CircularButton";
 export { default as TextField } from "./TextField";
 export { default as Form } from "./Form";
 export { default as Button } from "./Button";
+export { default as Select } from "./Select";
